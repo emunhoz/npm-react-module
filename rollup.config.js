1ae6541fe91afc9b93ba2fcf40e394068fb5da66
Eder Munhoz dos Santos <eder.santos@luizalabs.com>
2020-06-13 03:07:35 -0300

    chore: add config to export stylecomponent

--- rollup.config.js
+++ rollup.config.js
@@ -7,18 +7,29 @@ import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
+  external: ['styled-components'],
   output: [
     {
       file: pkg.main,
       format: 'cjs',
       exports: 'named',
-      sourcemap: true
+      sourcemap: true,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'styled-components': 'styled',
+      },
     },
     {
       file: pkg.module,
       format: 'es',
       exports: 'named',
-      sourcemap: true
+      sourcemap: true,
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        'styled-components': 'styled',
+      },
     }
   ],
   plugins: [
@@ -33,7 +44,7 @@ export default {
       clean: true
     }),
     commonjs({
-      include: ['node_modules/**'],
+      include: 'node_modules/**',
       namedExports: {
         'node_modules/react/react.js': [
           'Children',
@@ -41,7 +52,12 @@ export default {
           'PropTypes',
           'createElement'
         ],
-        'node_modules/react-dom/index.js': ['render']
+        'node_modules/react-dom/index.js': ['render', 'hydrate'],
+        'node_modules/react-is/index.js': [
+          'isElement',
+          'isValidElementType',
+          'ForwardRef'
+        ]
       }
     })
   ]
