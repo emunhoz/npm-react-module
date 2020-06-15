aeff75cb98b1142010780e5f37ef6af5085c3674
Eder Munhoz dos Santos <eder.santos@luizalabs.com>
2020-06-15 00:21:27 -0300

    style: add stylelint pattern

--- src/components/button/styled.ts
+++ src/components/button/styled.ts
@@ -1,32 +1,32 @@
 import styled, { css } from 'styled-components'
 
 const NegativeStyle = css`
-  background: #fff;
   color: #0086FF;
-  box-shadow: 0px 4px 20px rgba(155, 155, 155, 0.2);
+  background: #fff;
+  box-shadow: 0 4px 20px rgba(155, 155, 155, .2);
 
   :hover {
-    background: #0086FF;
     color: #fff;
+    background: #0086FF;
   }
 `
 
 export const Button = styled.button<{ negative: boolean;}>`
-  background: #0086FF;
-  border: 1px solid #4B83F9;
   box-sizing: border-box;
-  border-radius: 25px;
   width: 348px;
   height: 50px;
   color: #fff;
-  cursor: pointer;
   text-transform: uppercase;
-  transition: 0.3s;
-  box-shadow: 0px -2px 15px rgba(0, 115, 219, 0.2);
+  cursor: pointer;
+  background: ${({ theme }) => theme.palette.primary};
+  border: 1px solid #4B83F9;
+  border-radius: 25px;
+  box-shadow: 0 -2px 15px rgba(0, 115, 219, .2);
+  transition: .3s;
 
   :hover {
-    background: #fff;
     color: #4B83F9;
+    background: #fff;
   }
 
   ${({ negative }) => negative && NegativeStyle};
