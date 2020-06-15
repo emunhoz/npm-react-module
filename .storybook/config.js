5023eb037489fc140abef726329db4388e731494
Eder Munhoz dos Santos <eder.santos@luizalabs.com>
2020-06-15 02:31:17 -0300

    chore: add themeProvider to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,5 +4,13 @@ import { withA11y } from '@storybook/addon-a11y'
 import createTheme, { GlobalStyles } from '../src/theme'
 import { ThemeProvider } from 'styled-components';
 
+const styles = {
+  textAlign: 'center',
+  padding: '30px',
+  display: 'flex',
+  justifyContent: 'center',
+  margin: '0 auto'
+}
+
 addDecorator(withA11y)
-addDecorator(storyFn => <ThemeProvider theme={createTheme}><GlobalStyles />{storyFn()}</ThemeProvider>)
+addDecorator(storyFn => <ThemeProvider theme={createTheme}><GlobalStyles /><div style={styles}>{storyFn()}</div></ThemeProvider>)
