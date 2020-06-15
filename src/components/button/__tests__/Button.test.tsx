f38d5c32e5c9b1f8e77eb00c748f1c36771bfdca
Eder Munhoz dos Santos <eder.santos@luizalabs.com>
2020-06-15 02:31:49 -0300

    test(snapshot): update

--- src/components/button/__tests__/Button.test.tsx
+++ src/components/button/__tests__/Button.test.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
-import { render, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from 'test-utils'
 import { Button } from '../'
 
 describe('[Component]: Button', () => {
