c54b48f8edd56ad82ab99819323b6752d717aedb
Eder Munhoz dos Santos <eder.santos@luizalabs.com>
2020-06-17 23:09:03 -0300

    chore: remove square button component

--- src/components/square-button/SquareButton.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import * as S from './styled'
-
-interface ISquareButton {
-  children?: React.ReactNode
-  onClick: () => void
-}
-
-export const SquareButton: React.FC<ISquareButton> = ({ onClick, children }) => <S.SquareButton onClick={onClick}>{children}</S.SquareButton>
--- src/components/square-button/__tests__/SquareButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-import { render } from 'test-utils'
-import { SquareButton } from '../';
-
-describe('[Component]: SquareButton', () => {
-  it('render the component', () => {
-    expect(render(<SquareButton onClick={() => console.log}>Text</SquareButton>)).toMatchSnapshot();
-  });
-});
