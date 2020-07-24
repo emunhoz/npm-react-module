d45d460e969f48ee29a2dd5398e9fc33d7816aeb
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2020-07-24 01:36:47 -0300

    chore: update button component with new style system

--- src/components/button/styled.ts
+++ src/components/button/styled.ts
@@ -1,29 +1,29 @@
 import styled, { css } from 'styled-components'
 
 const NegativeStyle = css`
-  color: ${({ theme }) => theme.palette.black};
-  background: ${({ theme }) => theme.palette.white};
-  box-shadow: 0 0 46px 0 rgba(0, 0, 0, 0.13);
+  color: ${({ theme }) => theme.palette.darkest};
+  background: ${({ theme }) => theme.palette.lightest};
+  box-shadow: ${({ theme }) => theme.attributes.boxShadow.small};
 
   :hover {
-    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.13);
+    box-shadow: ${({ theme }) => theme.attributes.boxShadow.hover};
   }
 `
 
 export const Button = styled.button<{ negative: boolean }>`
   min-width: 320px;
   padding: 24px 40px;
-  font-size: ${({ theme }) => theme.typography.fontSizeSM};
-  font-weight: ${({ theme }) => theme.typography.fontWeightMedium};
-  color: ${({ theme }) => theme.palette.white};
+  font-size: ${({ theme }) => `${theme.typography.size.s3}px`};
+  font-weight: ${({ theme }) => theme.typography.weight.bold};
+  color: ${({ theme }) => theme.palette.lightest};
   cursor: pointer;
   background: ${({ theme }) => theme.palette.primary};
   border: none;
-  border-radius: ${({ theme }) => theme.attributes.borderRadius};
+  border-radius: ${({ theme }) => `${theme.attributes.borderRadius.big}px`};
   transition: 0.3s;
 
   :hover {
-    filter: brightness(150%);
+    filter: brightness(110%);
   }
 
   ${({ negative }) => negative && NegativeStyle};
