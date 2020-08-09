4bea479040a9029f4ef2b0e2d7ac64c1760c02a4
emunhoz <edermunhozsantos@gmail.com>
2020-08-09 03:46:46 -0300

    refactor: button component with new style vars

--- src/components/button/styled.ts
+++ src/components/button/styled.ts
@@ -3,23 +3,23 @@ import styled, { css } from 'styled-components'
 const NegativeStyle = css`
   color: ${({ theme }) => theme.palette.darkest};
   background: ${({ theme }) => theme.palette.lightest};
-  box-shadow: ${({ theme }) => theme.attributes.boxShadow.small};
+  box-shadow: 0 0 46px 0 rgba(0, 0, 0, 0.13);
 
   :hover {
-    box-shadow: ${({ theme }) => theme.attributes.boxShadow.hover};
+    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.13);
   }
 `
 
 export const Button = styled.button<{ negative: boolean }>`
   min-width: 320px;
   padding: 24px 40px;
-  font-size: ${({ theme }) => theme.typography.size.s3}px;
-  font-weight: ${({ theme }) => theme.typography.weight.bold};
+  font-size: ${({ theme }) => theme.fontSize.s3}px;
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
   color: ${({ theme }) => theme.palette.lightest};
   cursor: pointer;
   background: ${({ theme }) => theme.palette.primary};
   border: none;
-  border-radius: ${({ theme }) => `${theme.attributes.borderRadius.big}px`};
+  border-radius: 20px;
   transition: 0.3s;
 
   :hover {
